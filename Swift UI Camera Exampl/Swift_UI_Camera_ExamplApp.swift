3799fdeee8d03fac5606bf111611e57f62b58609
nathangendler <106348682+nathangendler@users.noreply.github.com>
2023-03-17 09:44:55 -0700

    initial commit

---
+++ Swift UI Camera Exampl/Swift_UI_Camera_ExamplApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_Camera_ExamplApp.swift
+//  Swift UI Camera Exampl
+//
+//  Created by Nathan Gendler on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_Camera_ExamplApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
